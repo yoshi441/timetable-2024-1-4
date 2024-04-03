f9d9ad7feb6ed9768154ee8f55ea54519ee02a84
Haru-0205 <rm23081b@st.omu.ac.jp>
2024-04-03 12:43:01 +0900

    update components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import timetable from "@/components/timetable.ts"
 
 export default function Home() {
   return (
---
+++ src/components/header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return(
+    <>
+      <div className={"bg-green-500 h-20 w-full"}>
+        <h1 className={"text-2xl text-slate-800"}>
+          2024年度2年Eコース掲示板
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default Header;
