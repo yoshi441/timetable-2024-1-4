145d2119865f187ffd0e4ea8089edf6b539185db
Haru-0205 <rm23081b@st.omu.ac.jp>
2024-04-03 19:32:49 +0900

    add issue template

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,19 @@
 import React from "react";
+import '@/styles/header.css'
 
 const Header = () => {
   return(
     <>
-      <div className={"bg-green-500 h-20 w-full"}>
-        <h1 className={"text-2xl text-slate-800"}>
+      <div className={"bg-green-300 h-20 p-5 w-full grid grid-init-pc"}>
+        <h1 className={"text-3xl text-slate-700"}>
           2024年度2年Eコース掲示板
         </h1>
+        <p>Top</p>
+        <p>About</p>
+        <p>TimeTable</p>
+        <p>ClassCalender</p>
+        <p>Links</p>
+        <p>Appendix</p>
       </div>
     </>
   )
